335cd8141287997f5078c471799559a868c65661
Vinh Nguyen <zinh@users.noreply.github.com>
2022-12-31 16:47:08 +0900

    day 1 part 2

--- day01/part1.clj
+++ day01/part1.clj
@@ -2,13 +2,14 @@
 
 (require '[clojure.str :as str])
 
-
 (defn sum
   [line]
-  (reduce (fn [total item] (+ total (Integer. item))) 0 (str/split line #"\n"))
-  )
+  (reduce (fn [total item] (+ total (Integer. item))) 0 (str/split line #"\n")))
 
 (defn process
   [filename]
-  (map sum (str/split (slurp filename) #"\n\n"))
-  )
+  (map sum (str/split (slurp filename) #"\n\n")))
+
+(defn sumTopN
+  [filename n]
+  (reduce + (take n (reverse (sort (map sum (str/split (slurp filename) #"\n\n")))))))
